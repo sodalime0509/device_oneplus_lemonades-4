93ec6337dbfa5a401fcdc510e195abd3f6b6cc75
AnierinB <anierin@evolution-x.org>
2022-07-21 18:18:37 +0000

    lemonades: Initialize Evolution X 7.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+#               2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonades.mk
+    $(LOCAL_DIR)/evolution_lemonades.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonades-user \
-    lineage_lemonades-userdebug \
-    lineage_lemonades-eng
+    evolution_lemonades-user \
+    evolution_lemonades-userdebug \
+    evolution_lemonades-eng
