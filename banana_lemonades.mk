6a2d90ad19c6624f54d5b52e3e7df520b99bb05f
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-06-10 11:44:39 +0530

    Update and rename evolution_lemonades.mk to banana_lemonades.mk

--- banana_lemonades.mk
+++ banana_lemonades.mk
@@ -13,18 +13,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common_full_phone.mk)
 
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := evolution_lemonades
+PRODUCT_NAME := banana_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2101
 
+# BananaDroid
+BANANA_MAINTAINER := Nacho
+WITH_GAPPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGER_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_EXCLUDE_MATLOG := true
+TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_SYSTEM_NAME := OnePlus9R
 PRODUCT_SYSTEM_DEVICE := OnePlus9R
 
