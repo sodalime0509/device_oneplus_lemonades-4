34ac52cad859355de5628836dd1b1a44f34468e5
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-06-14 11:47:15 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_lemonades.mk
+    $(LOCAL_DIR)/derp_lemonades.mk
 
 COMMON_LUNCH_CHOICES := \
-    banana_lemonades-user \
-    banana_lemonades-userdebug \
-    banana_lemonades-eng
+    derp_lemonades-user \
+    derp_lemonades-userdebug \
+    derp_lemonades-eng
